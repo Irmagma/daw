e47fd906ccb4e3c5d8d4f670b8b9b65ea4b87c63
Irmagma <31437226+Irmagma@users.noreply.github.com>
2017-12-10 23:36:23 +0000

    Update init.pp

--- puppet/modules/daw/manifests/init.pp
+++ puppet/modules/daw/manifests/init.pp
@@ -3,6 +3,10 @@ class daw {
         ensure => 'installed',
 	allowcdrom => true,      
  }
+ 	exec { 'apt-get update':
+	 command => '/usr/bin/apt-get update',
+	 refreshonly => true,
+ }	
 	file { '/home/xubuntu/lmms':
 	 ensure => 'directory',
  }
