f1c875a07409ff457ebec4eff4ab45dfb96fa278
Irmagma <tomi.irla@gmail.com>
2017-12-10 20:50:26 +0000

    add daw

--- puppet/modules/daw/manifests/init.pp
+++ puppet/modules/daw/manifests/init.pp
@@ -3,4 +3,16 @@ class daw {
         ensure => 'installed',
 	allowcdrom => true,      
  }
+	file { '/home/xubuntu/lmms':
+	 ensure => 'directory',
+ }
+	file { '/home/xubuntu/lmms/presets':
+         ensure => 'directory',
+ }
+	file { '/home/xubuntu/lmms/projects':
+         ensure => 'directory',
+ }	
+	file { '/home/xubuntu/lmms/samples':
+         ensure => 'directory',
+ }
 }
