1cb26a6a06a205df16e71c0ebdffd20e08f061af
Irmagma <tomi.irla@gmail.com>
2017-12-11 03:18:38 +0000

    add daw

--- puppet/modules/daw/manifests/init.pp
+++ puppet/modules/daw/manifests/init.pp
@@ -1,9 +1,22 @@
 class daw {
-	package { "lmms":
-        ensure => 'installed',
-	allowcdrom => true,      
+
+	exec {'retrieve_kxstudio-repos_9.4.6~kxstudio1_all.deb':
+	 command => "/usr/bin/wget -q https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_9.4.6~kxstudio1_all.deb -O /home/xubuntu/kxstudio-repos_9.4.6~kxstudio1_all.deb",
  }
- 	exec { 'apt-get update':
+	exec {'retrieve_kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb':
+	 command => "/usr/bin/wget -q https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb -O /home/xubuntu/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb",
+ }
+	exec {'sudo dpkg':
+	 command => "/usr/bin/dpkg -i /home/xubuntu/kxstudio-repos_9.4.6~kxstudio1_all.deb",
+ }
+	exec {'sudo dpkg':
+	 command => "/usr/bin/dpkg -i /home/xubuntu/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb",
+ }
+	package { "lmms":
+         ensure => 'installed',
+	 allowcdrom => true,      
+ }
+	exec { 'apt-get update':
 	 command => '/usr/bin/apt-get update',
 	 refreshonly => true,
  }	
@@ -19,4 +32,5 @@ class daw {
 	file { '/home/xubuntu/lmms/samples':
          ensure => 'directory',
  }
+
 }
