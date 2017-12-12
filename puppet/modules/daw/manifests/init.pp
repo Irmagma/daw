b1d4e7dcd274b3343494640ba628f929c695a749
Irmagma <31437226+Irmagma@users.noreply.github.com>
2017-12-12 10:38:41 +0000

    Update init.pp

--- puppet/modules/daw/manifests/init.pp
+++ puppet/modules/daw/manifests/init.pp
@@ -1,35 +1,35 @@
 class daw {
 
 	exec {'retrieve_kxstudio-repos_9.4.6~kxstudio1_all.deb':
-	 command => "/usr/bin/wget -q https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_9.4.6~kxstudio1_all.deb -O /home/xubuntu/kxstudio-repos_9.4.6~kxstudio1_all.deb",
- 	 before => Exec['sudo dpkg'],
+		command => "/usr/bin/wget -q https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_9.4.6~kxstudio1_all.deb -O /home/xubuntu/kxstudio-repos_9.4.6~kxstudio1_all.deb",
+ 		before => Exec['sudo dpkg'],
 	}		 
 	exec {'retrieve_kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb':
-	 command => "/usr/bin/wget -q https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb -O /home/xubuntu/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb",
-	 before => Exec['sudo dpkg'],	
+		command => "/usr/bin/wget -q https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb -O /home/xubuntu/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb",
+		before => Exec['sudo dpkg'],	
 	}
 	exec {'sudo dpkg':
-	 command => "/usr/bin/dpkg -i /home/xubuntu/kxstudio-repos_9.4.6~kxstudio1_all.deb ; /usr/bin/dpkg -i /home/xubuntu/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb", 
+		command => "/usr/bin/dpkg -i /home/xubuntu/kxstudio-repos_9.4.6~kxstudio1_all.deb ; /usr/bin/dpkg -i /home/xubuntu/kxstudio-repos-gcc5_9.4.6~kxstudio1_all.deb", 
 	}
 	file { '/home/xubuntu/lmms':
-	 ensure => 'directory',
+		ensure => 'directory',
 	}
 	file { '/home/xubuntu/lmms/presets':
-         ensure => 'directory',
+        	ensure => 'directory',
 	}
 	file { '/home/xubuntu/lmms/projects':
-         ensure => 'directory',
+        	ensure => 'directory',
 	}	
 	file { '/home/xubuntu/lmms/samples':
-         ensure => 'directory',
+        	ensure => 'directory',
 	}
  	exec { 'apt-get update':
-	 command => '/usr/bin/apt-get update',
-	 subscribe => Exec['sudo dpkg'],
-	 notify => Package['lmms'],
+		command => '/usr/bin/apt-get update',
+		subscribe => Exec['sudo dpkg'],
+		notify => Package['lmms'],
 	}
 	package { "lmms":
-         ensure => 'installed',
-	 allowcdrom => true,
+        	ensure => 'installed',
+		allowcdrom => true,
 	}
 }
